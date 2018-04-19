4b47e8066d851c0c08170820c6a601b846677e74
Andresbottero89 <38111038+Andresbottero89@users.noreply.github.com>
2018-04-19 15:04:18 -0300

    Update adicion.cpp

--- 01-adicion/adicion.cpp
+++ 01-adicion/adicion.cpp
@@ -7,11 +7,12 @@
 using namespace std;
 
 int main(){
+  
 int num1,num2;
 cout<<"01-ADICION\nIngrese un numero: ";
 cin>>num1;
 cout<<"\nIngrese otro numero: ";
 cin>>num2;
-cout<<"\n\n"<<num1<<"+"<<num2<<"="<<num1+num2;
+cout<<num1<<"+"<<num2<<"="<<num1+num2;
 
 }
