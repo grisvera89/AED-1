034ed93cd3a67d346c66e925ae3ac4fb9f84842a
Andresbottero89 <38111038+Andresbottero89@users.noreply.github.com>
2018-04-19 15:03:30 -0300

    Create adicion.cpp

---
+++ 01-adicion/adicion.cpp
@@ -0,0 +1,17 @@
+/* 
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int num1,num2;
+cout<<"01-ADICION\nIngrese un numero: ";
+cin>>num1;
+cout<<"\nIngrese otro numero: ";
+cin>>num2;
+cout<<"\n\n"<<num1<<"+"<<num2<<"="<<num1+num2;
+
+}
